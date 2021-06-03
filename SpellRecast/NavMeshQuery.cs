97302f67549786c26b3cc2baadf67bdee505507d
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-06-03 17:06:23 -0400

    Path smoothing

---
+++ SpellRecast/NavMeshQuery.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Numerics;
+using RecastSharp.DetourNative;
+
+namespace SpellRecast
+{
+    public class NavMeshQuery
+    {
+        public IntPtr Pointer;
+        private IntPtr filter;
+
+        private float[] tempArray = new float[3];
+        private float polyFindRange = 3;
+        private float[] nearestArray = new float[3];
+        private float[] closest = new float[3];
+        private bool tempBool = false;
+        int maxPath = 32;
+
+        private float[] StartPos = new float[3];
+        private float[] EndPos = new float[3];
+
+        public NavMeshQuery(ref NavMesh navMesh)
+        {
+            Pointer = DetourNative.dtwAllocNavMeshQuery();
+            DetourNative.dtwNavMeshQuery_Init(Pointer, navMesh.Pointer, 2048);
+            filter = DetourNative.dtwAllocQueryFilter();
+        }
+
+        public DtwStatus FindNearestPoly(Vector3 pos, out int nearestPoly)
+        {
+            nearestPoly = 0;
+            tempArray[0] = pos.X;
+            tempArray[1] = pos.Y;
+            tempArray[2] = pos.Z;
+            DtwStatus result = DetourNative.dtwNavMeshQuery_FindNearestPoly(Pointer, ref tempArray[0], ref polyFindRange, filter, ref nearestPoly, ref nearestArray[0]);
+            return result;
+        }
+
+        public Vector3 FindNearestPointBoundary(Vector3 pos, int polyRef)
+        {
+            tempArray[0] = pos.X;
+            tempArray[1] = pos.Y;
+            tempArray[2] = pos.Z;
+            var result =  DetourNative.dtwNavMeshQuery_closestPointOnPolyBoundary(Pointer, polyRef, ref tempArray[0], ref closest[0]);
+            return result == DtwStatus.Success ? new Vector3(closest[0], closest[1], closest[2]) : Vector3.Zero;
+        }
+        
+        public Vector3 FindNearestPoint(Vector3 pos, int polyRef)
+        {
+            tempArray[0] = pos.X;
+            tempArray[1] = pos.Y;
+            tempArray[2] = pos.Z;
+            var result =  DetourNative.dtwNavMeshQuery_closestPointOnPoly(Pointer, polyRef, ref tempArray[0], ref closest[0], ref tempBool);
+            return result == DtwStatus.Success ? new Vector3(closest[0], closest[1], closest[2]) : Vector3.Zero;
+        }
+
+        //var pathResult = DetourNative.dtwNavMeshQuery_FindPath(naviMeshQuery, spos, epos, ref startArray[0], ref endArray[0], filter, ref navPath[0], out int pathCount, maxPath);
+
+        public DtwStatus FindPath(Vector3 start, Vector3 end, int maxPath, out int[] path)
+        {
+            int[] tempPath = new int[maxPath];
+            int spos = -1;
+            int epos = -1;
+            path = null;
+            
+            if (FindNearestPoly(start, out spos) != DtwStatus.Success)
+            {
+                return DtwStatus.Failure;
+            }
+
+            //Console.WriteLine($"{tempArray[0]} {tempArray[1]} {tempArray[2]} ");
+            if (FindNearestPoly(end, out epos) != DtwStatus.Success)
+            {
+                return DtwStatus.Failure;
+            }
+
+            SetStart(start);
+            SetEnd(end);
+            //Console.WriteLine($"{tempArray[0]} {tempArray[1]} {tempArray[2]} ");
+            
+            var pathResult = DetourNative.dtwNavMeshQuery_FindPath(this.Pointer, spos, epos, ref StartPos[0], ref EndPos[0], filter, ref tempPath[0], out int pathCount, maxPath);
+
+            if (pathResult == DtwStatus.Success)
+            {
+                path = new int[pathCount];
+                for (var i = 0; i < pathCount; i++)
+                {
+                    path[i] = tempPath[i];
+                }
+            }
+
+            return pathResult;
+        }
+
+        
+        public DtwStatus FindSmoothPath(Vector3 start, Vector3 end, int maxPath, out Vector3[] pathout)
+        {
+            var result = FindPath(start, end, 100, out var path);
+
+            float[] smoothpath = new float[maxPath * 3];
+            int smoothPathLength = 0;
+            
+            if (result != DtwStatus.Success)
+            {
+                pathout = null;
+                return DtwStatus.Failure;
+            }
+
+            SetStart(start);
+            SetEnd(end);
+
+            result = DetourNative.findSmoothPath(this.Pointer, this.filter, ref StartPos[0], ref EndPos[0], ref path[0], path.Length, ref smoothpath[0], ref smoothPathLength, maxPath);
+
+            Console.WriteLine($"{result} {smoothPathLength}");
+            pathout = new Vector3[smoothPathLength];
+            int pos = 0;
+            for (int i = 0; i < smoothPathLength; i++)
+            {
+                pathout[i] = new Vector3(smoothpath[pos], smoothpath[pos + 1], smoothpath[pos +2]);
+                pos += 3;
+            }
+            return result;
+        }
+
+        private void SetStart(Vector3 pos)
+        {
+            StartPos[0] = pos.X;
+            StartPos[1] = pos.Y;
+            StartPos[2] = pos.Z;
+        }
+        
+        private void SetEnd(Vector3 pos)
+        {
+            EndPos[0] = pos.X;
+            EndPos[1] = pos.Y;
+            EndPos[2] = pos.Z;
+        }
+    }
+}
--- SpellRecastTester/Program.cs
+++ SpellRecastTester/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
 using System.Reflection;
+using RecastSharp.DetourNative;
 using SpellRecast;
 using SpellRecast.NavFileHelper;
 
@@ -11,15 +13,7 @@ namespace SpellRecastTester
     {
         public static void Main(string[] args1)
         {
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
-                String resourceName = "AssemblyLoadingAndReflection." +
-                                      new AssemblyName(args.Name).Name + ".dll";
-                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)) {
-                    Byte[] assemblyData = new Byte[stream.Length];
-                    stream.Read(assemblyData, 0, assemblyData.Length);
-                    return Assembly.Load(assemblyData);
-                }
-            };
+
             NavMesh navMesh; // = DetourCommon.dtAllocNavMesh();
             var path = "d2t1.nav";
 
@@ -29,36 +23,83 @@ namespace SpellRecastTester
                 Console.WriteLine($"Error reading {path}");
             }
 
-            float polyFindRange = 3;
-            float[] nearestArray = new float[3];
-            int nearestRef = 0;
-            int spos = 0, epos = 0;
+            NavMeshQuery navMeshQuery = new NavMeshQuery(ref navMesh);
+            
+             Vector3 start = new Vector3(80.63416f, 207.3399f, -29.44104f);
+             Vector3 end = new Vector3( 94.16319f, 215f, -122.0762f );
 
-            /*IntPtr naviMeshQuery = DetourNative.dtwAllocNavMeshQuery();
-            DetourNative.dtwNavMeshQuery_Init(naviMeshQuery, navMesh.Pointer, 2048);
+             var resultPath = navMeshQuery.FindSmoothPath(start, end, 100, out var path1);
+             
+             Console.WriteLine($"{resultPath} - Path Length: {path1.Length}");
+
+             for (var index = 0; index < path1.Length; index++)
+             {
+                 var vector3 = path1[index];
+                 if (index + 1 < path1.Length)
+                 {
+                     //Console.WriteLine($"{Vector3.Distance(vector3, path1[index + 1])}");
+                     
+                 }
+
+                 Console.WriteLine($"new Vector3({vector3.X}f, {vector3.Y}f, {vector3.Z}f),");
+                 //Console.WriteLine(vector3);
+             }
+             //var resultStart = navMeshQuery.FindNearestPoly(start, out var spos);
+
+             //Console.WriteLine($"{resultStart} - {spos}");
+
+             //resultStart = navMeshQuery.FindNearestPoly(end, out var epos);
+
+             // Console.WriteLine($"{resultStart} - {epos}");
+             //var resultPath = navMeshQuery.FindPath(start, end, 100, out var navPath);
+             /*
+             Console.WriteLine($"{resultPath} - Path Length: {navPath.Length}");
+ 
+ 
+             float[] startPos = new[] { 80.63416f, 207.3399f, -29.44104f };
+             float[] closest = new float[3];
+             bool overPoly = false;
+ 
+             DtwStatus result;
+             //result = DetourNative.dtwNavMeshQuery_closestPointOnPoly(navMeshQuery.Pointer, navPath[0], ref startPos[0], ref closest[0], ref overPoly);
+             //Console.WriteLine($"{result} {closest[0]}, {closest[1]}, {closest[2]} {overPoly}");
+             Console.WriteLine($"new Vector3({start.X}f, {start.Y}f, {start.Z}f),");
+             foreach (var polyRef in navPath)
+             {
+                 start = navMeshQuery.FindNearestPoint(start, polyRef);
+                 Console.WriteLine($"new Vector3({start.X}f, {start.Y}f, {start.Z}f),");
+             }
+             Console.WriteLine($"new Vector3({end.X}f, {end.Y}f, {end.Z}f)");
+             */
 
 
-            // Vector3 start = new Vector3(80.63416f, 207.3399f, -29.44104f);
-            // Vector3 end = new Vector3(36.01461f, 208.2336f, -51.62287f);
-
-            //-78.33846f, 205.784f, -98.63499f
-            //94.16319f, 215f, -122.0762f
-
-            float[] startArray = new float[] { 80.63416f, 207.3399f, -29.44104f };
-            float[] endArray = new float[] { 94.16319f, 215f, -122.0762f };
-            int maxPath = 32;
-            int[] navPath = new int[maxPath];
-
-            var filter = DetourNative.dtwAllocQueryFilter();
-
-            var result = DetourNative.dtwNavMeshQuery_FindNearestPoly(naviMeshQuery, ref startArray[0], ref polyFindRange, filter, ref spos, ref nearestArray[0]);
-            Console.WriteLine($"Start Location: {result} - poly#: {spos} Nearest point ({nearestArray[0]} {nearestArray[1]} {nearestArray[2]})");
-            result = DetourNative.dtwNavMeshQuery_FindNearestPoly(naviMeshQuery, ref endArray[0], ref polyFindRange, filter, ref epos, ref nearestArray[0]);
-            Console.WriteLine($"Ending Location: {result} - poly#: {epos} Nearest point ({nearestArray[0]} {nearestArray[1]} {nearestArray[2]})");
-
-            var pathResult = DetourNative.dtwNavMeshQuery_FindPath(naviMeshQuery, spos, epos, ref startArray[0], ref endArray[0], filter, ref navPath[0], out int pathCount, maxPath);
-            Console.WriteLine($"Path Generation: {pathResult}");
-            Console.WriteLine($"Num of polygons in path: {pathCount}");*/
+             // var nextPoint = navMeshQuery.FindNearestPointBoundary(start, navPath[0]);
+             // Console.WriteLine(nextPoint);
+             /*IntPtr naviMeshQuery = DetourNative.dtwAllocNavMeshQuery();
+             DetourNative.dtwNavMeshQuery_Init(naviMeshQuery, navMesh.Pointer, 2048);
+ 
+ 
+             // Vector3 start = new Vector3(80.63416f, 207.3399f, -29.44104f);
+             // Vector3 end = new Vector3(36.01461f, 208.2336f, -51.62287f);
+ 
+             //-78.33846f, 205.784f, -98.63499f
+             //94.16319f, 215f, -122.0762f
+ 
+             float[] startArray = new float[] { 80.63416f, 207.3399f, -29.44104f };
+             float[] endArray = new float[] { 94.16319f, 215f, -122.0762f };
+             int maxPath = 32;
+             int[] navPath = new int[maxPath];
+ 
+             var filter = DetourNative.dtwAllocQueryFilter();
+ 
+             var result = DetourNative.dtwNavMeshQuery_FindNearestPoly(naviMeshQuery, ref startArray[0], ref polyFindRange, filter, ref spos, ref nearestArray[0]);
+             Console.WriteLine($"Start Location: {result} - poly#: {spos} Nearest point ({nearestArray[0]} {nearestArray[1]} {nearestArray[2]})");
+             result = DetourNative.dtwNavMeshQuery_FindNearestPoly(naviMeshQuery, ref endArray[0], ref polyFindRange, filter, ref epos, ref nearestArray[0]);
+             Console.WriteLine($"Ending Location: {result} - poly#: {epos} Nearest point ({nearestArray[0]} {nearestArray[1]} {nearestArray[2]})");
+ 
+             var pathResult = DetourNative.dtwNavMeshQuery_FindPath(naviMeshQuery, spos, epos, ref startArray[0], ref endArray[0], filter, ref navPath[0], out int pathCount, maxPath);
+             Console.WriteLine($"Path Generation: {pathResult}");
+             Console.WriteLine($"Num of polygons in path: {pathCount}");*/
         }
     }
 }
